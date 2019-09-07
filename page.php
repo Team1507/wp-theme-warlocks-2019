202e87ad16d3548cc8167d25fb078827b6a79a1f
Adam Stockton <adamstockton22@gmail.com>
2019-09-07 17:25:54 -0400

    Fixed Centering for H1-H4 Tags on Pages

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 <br>
         <div class="container">
             <div class="row">
-                <div class="col-xs-12 col-lg-12">
+                <div class="col-xs-12 col-lg-12 page-content">
                     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
                     <?php the_content(); ?>
                     <?php endwhile; ?>
